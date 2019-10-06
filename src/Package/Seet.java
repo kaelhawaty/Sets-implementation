30d9ced298d986dbdc4d3e46cced613497a704fb
Karim Ahmed <kaelhawaty@gmail.com>
2019-10-06 16:20:34 +0200

    V2

--- src/Package/Seet.java
+++ src/Package/Seet.java
@@ -1,5 +1,23 @@
 package Package;
-
+import java.util.*;
+// hello
 public class Seet {
-
+	private Set<String> hash_set = new HashSet<String>();
+	private ArrayList<String> set;
+	Seet(ArrayList<String> initial){
+		set = initial;
+		for(int i = 0; i < set.size(); i++) {
+			hash_set.add(set.get(i));
+		}
+	}
+	public void print() {
+		System.out.print("{");
+		for(int i = 0; i < set.size(); i++) {
+			System.out.print(set.get(i) + " ");
+		}
+		System.out.print("}");	
+	}
+	public boolean exist(String s) {
+		return hash_set.contains(s);
+	}
 }
